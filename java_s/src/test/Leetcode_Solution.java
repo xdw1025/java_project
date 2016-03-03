b36481868d357e2a0f4d6531f9e19c97e98ad0a9
xdw1025 <1072714062@qq.com>
2016-03-03 22:28:09 +0800

    majorityElement and happyNumber

--- java_s/src/test/Leetcode_Solution.java
+++ java_s/src/test/Leetcode_Solution.java
@@ -7,13 +7,52 @@ public class Leetcode_Solution {
 			if(!numbers.add(i)){return true;}
 		}
 		return false;
-		
+		//判断一个数组里是否有重复的数
 	}
 	
+	
+	public static int titleToNumber(String s) {
+		int result = 0; 
+		for (int i = 0; i < s.length(); i++) 
+		{ result = result * 26 + (s.charAt(i) - 'A' + 1); } 
+		return result;
+		//将一个26进制的数转换为十进制的数，A->1 B->2...Z->26 AA->27 AB->28
+       
+    }
+
+	public static int majorityElement(int[] nums) {
+		int n=nums.length;
+		Arrays.sort(nums);
+		return nums[n/2];
+    //输出一个数组中出现次数超过一半的数
+	}
+	
+	 public static boolean isHappy(int n) {
+	        Set<Integer> numberSet = new HashSet<Integer>();
+	         
+	        while (n!=1){
+	            if (numberSet.add(n)==false){
+	                return false;
+	            }
+	            char[] chars = String.valueOf(n).toCharArray();
+	            int total = 0;
+	            for (Character ch: chars){
+	                total += Math.pow(Integer.parseInt(ch+""),2);
+	            }
+	            n = total;
+	        }
+	        return true;
+	        //判断一个数是不是happyNumber ex 19 is a happy number
+	        //1^2 + 9^2 = 82
+	        		//8^2 + 2^2 = 68
+	        		//6^2 + 8^2 = 100
+	        		//1^2 + 0^2 + 0^2 = 1 结果为1   要不会进入一个数重复的循环
+	    }
+
 	public static void main (String[] args) {
-		int[] nums={12,2,3,12,4,5};
-		
-		boolean x=containDuplicate(nums);
+		int n=87;
+		boolean x;
+		x=isHappy(n);
 		System.out.println(x);
 		
 	}
